69c9d648759c2f2f359a9ab44c75008c89c8867e
V.431_ <waelabdelal7@gmail.com>
2024-08-31 15:22:09 +0300

    dark/light modes

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -11,8 +11,10 @@ public class GameManager : MonoBehaviour
 {
     public Button[] gridSpaces;
     public Button resetButton;
+    public Button uiModeButton;
     public TextMeshProUGUI turnIndicatorText;
     public TextMeshProUGUI timerText;
+    public TextMeshProUGUI logoText;
     public AudioSource clickSFX;
     public AudioSource popSFX;
     public AudioSource resetSFX;
@@ -20,18 +22,40 @@ public class GameManager : MonoBehaviour
     public AudioSource drawSFX;
     public AudioSource soundtrack;
     public ParticleSystem confettiParticleSystem;
+    public Image waelLogo;
+    public Image bgImage;
+    public Image gameLogo;
+    public Camera mainCamera;
     private bool isGameActive;
     private string currentPlayer;
     private string[] boardState;
     private float gameTime;
     private Color xColor;
     private Color oColor;
+    private const string xChar = "1";
+    private const string oChar = "0";
+
+    // Dark/Light mode
+    private Color darkModeCameraColor;
+    private Color darkModeButtonColor;
+    private Color darkModeTurnTextColor;
+    private Color darkModeTimerTextColor;
+    private Color darkModeBGColor;
+    private Color darkModeLogoColor;
+
+    private Color lightModeCameraColor = new Color(1f, 1f, 1f); // FFFFFF
+    private Color lightModeButtonColor = new Color(0.773f, 0.773f, 0.773f); // C5C5C5
+    private Color lightModeTurnTextColor = new Color(0.773f, 0.773f, 0.773f); // C5C5C5
+    private Color lightModeTimerTextColor = new Color(0.773f, 0.773f, 0.773f); // C5C5C5
+    private Color lightModeBGColor = new Color(0.957f, 0.957f, 0.957f); // F4F4F4
+    private Color lightModeLogoColor = new Color(0.918f, 0.918f, 0.918f); // EAEAEA
+    private bool isDarkMode = true;
 
 
     void Start()
     {
         LeanTween.init(9900);
-        currentPlayer = "X";
+        currentPlayer = xChar;
         boardState = new string[9];
         SetTurnIndicatorColor("#616161");
         turnIndicatorText.text = "Player " + currentPlayer + "'s Turn";
@@ -39,6 +63,13 @@ public class GameManager : MonoBehaviour
         soundtrack.Play();
 
         SetRandomColors();
+
+        darkModeCameraColor = mainCamera.backgroundColor;
+        darkModeButtonColor = gridSpaces[0].GetComponent<Image>().color;
+        darkModeTurnTextColor = turnIndicatorText.color;
+        darkModeTimerTextColor = timerText.color;
+        darkModeBGColor = bgImage.color;
+        darkModeLogoColor = waelLogo.color;
 
         foreach (Button button in gridSpaces)
         {
@@ -54,11 +85,13 @@ public class GameManager : MonoBehaviour
 
         AnimateButtonsIn();
         resetButton.onClick.AddListener(ResetGame);
+        uiModeButton.onClick.AddListener(ToggleMode);
 
         gameTime = 0f;
         isGameActive = true;
         timerText.text = "00:000";
     }
+
 
     void Update()
     {
@@ -141,7 +174,7 @@ public class GameManager : MonoBehaviour
             var textComponent = gridSpaces[index].GetComponentInChildren<TextMeshProUGUI>();
             textComponent.text = currentPlayer;
 
-            if (currentPlayer == "X")
+            if (currentPlayer == xChar)
             {
                 textComponent.color = xColor;
             }
@@ -154,7 +187,11 @@ public class GameManager : MonoBehaviour
 
             if (CheckForWin(out int[] winIndices))
             {
-                SetTurnIndicatorColor("#ffffff");
+                if (isDarkMode)
+                    SetTurnIndicatorColor("#ffffff");
+                else
+                    SetTurnIndicatorColor("#555555");
+
                 turnIndicatorText.text = "Player " + currentPlayer + " Wins!";
                 FlashButtons(winIndices);
                 DisableButtons();
@@ -166,7 +203,11 @@ public class GameManager : MonoBehaviour
             }
             else if (CheckForDraw())
             {
-                SetTurnIndicatorColor("#ffffff");
+                if (isDarkMode)
+                    SetTurnIndicatorColor("#ffffff");
+                else
+                    SetTurnIndicatorColor("#555555");
+
                 turnIndicatorText.text = "It's a Draw!";
                 FlashButtons(null);
                 drawSFX.Play();
@@ -184,8 +225,8 @@ public class GameManager : MonoBehaviour
 
     void SwitchPlayer()
     {
-        currentPlayer = (currentPlayer == "X") ? "O" : "X";
-        SetTurnIndicatorColor("#616161");
+        currentPlayer = (currentPlayer == xChar) ? oChar : xChar;
+
         turnIndicatorText.text = "Player " + currentPlayer + "'s Turn";
     }
 
@@ -240,7 +281,12 @@ public class GameManager : MonoBehaviour
     void SetRandomColors()
     {
         float randomHueX = Random.Range(0f, 1f);
-        float randomHueO = Random.Range(0f, 1f);
+        float randomHueO;
+
+        do
+        {
+            randomHueO = Random.Range(0f, 1f);
+        } while (Mathf.Abs(randomHueX - randomHueO) < 0.09f);
 
         xColor = Color.HSVToRGB(randomHueX, 0.99f, 0.83f);
         oColor = Color.HSVToRGB(randomHueO, 0.99f, 0.83f);
@@ -249,11 +295,17 @@ public class GameManager : MonoBehaviour
         oColor.a = 1f;
     }
 
+
     public void ResetGame()
     {
-        currentPlayer = "X";
+        currentPlayer = xChar;
         boardState = new string[9];
-        SetTurnIndicatorColor("#616161");
+
+        if (isDarkMode)
+            SetTurnIndicatorColor("#616161");
+        else
+            SetTurnIndicatorColor("#C5C5C5");
+
         turnIndicatorText.text = "Player " + currentPlayer + "'s Turn";
 
         SetRandomColors();
@@ -282,10 +334,11 @@ public class GameManager : MonoBehaviour
             LeanTween.scale(gridSpaces[index].gameObject, Vector3.one, 0.28901f)
                      .setDelay(index * 0.11f)
                      .setEaseOutBounce()
+                     .setOnStart(() => popSFX.Play())
                      .setOnComplete(() =>
                      {
                          WobbleButton(gridSpaces[index]);
-                         popSFX.Play();
+                         //popSFX.Play();
                      });
         }
     }
@@ -360,4 +413,76 @@ public class GameManager : MonoBehaviour
             turnIndicatorText.color = color;
         }
     }
+
+    public void ToggleMode()
+    {
+        Color targetCameraColor = isDarkMode ? lightModeCameraColor : darkModeCameraColor;
+        Color targetButtonColor = isDarkMode ? lightModeButtonColor : darkModeButtonColor;
+        Color targetTurnTextColor = isDarkMode ? lightModeTurnTextColor : darkModeTurnTextColor;
+        Color targetTimerTextColor = isDarkMode ? lightModeTimerTextColor : darkModeTimerTextColor;
+        Color targetBGColor = isDarkMode ? lightModeBGColor : darkModeBGColor;
+        Color targetLogoColor = isDarkMode ? lightModeLogoColor : darkModeLogoColor;
+        Color targetGameLogoColor = isDarkMode ? new Color(1f, 1f, 1f, 1f) : new Color(0f, 0f, 0f, 0.6f);
+
+        LeanTween.value(gameObject, mainCamera.backgroundColor, targetCameraColor, 0.15f)
+                 .setOnUpdate((Color color) => { mainCamera.backgroundColor = color; });
+
+        foreach (Button button in gridSpaces)
+        {
+            LeanTween.value(button.gameObject, button.GetComponent<Image>().color, targetButtonColor, 0.15f)
+                     .setOnUpdate((Color color) => { button.GetComponent<Image>().color = color; });
+
+            LeanTween.scale(button.gameObject, Vector3.one * 1.05f, 0.1f)
+                     .setEaseOutQuad()
+                     .setLoopPingPong(1);
+        }
+
+        LeanTween.value(gameObject, turnIndicatorText.color, targetTurnTextColor, 0.15f)
+                 .setOnUpdate((Color color) => { turnIndicatorText.color = color; });
+
+        /*LeanTween.scale(turnIndicatorText.gameObject, Vector3.one * 1.05f, 0.1f)
+                 .setEaseOutQuad()
+                 .setLoopPingPong(1);
+*/
+        LeanTween.value(gameObject, timerText.color, targetTimerTextColor, 0.15f)
+                 .setOnUpdate((Color color) => { timerText.color = color; });
+
+/*        LeanTween.scale(timerText.gameObject, Vector3.one * 1.05f, 0.1f)
+                 .setEaseOutQuad()
+                 .setLoopPingPong(1);*/
+
+        LeanTween.value(bgImage.gameObject, bgImage.color, targetBGColor, 0.15f)
+                 .setOnUpdate((Color color) => { bgImage.color = color; });
+/*
+        LeanTween.scale(bgImage.gameObject, Vector3.one * 1.05f, 0.1f)
+                 .setEaseOutQuad()
+                 .setLoopPingPong(1);*/
+
+        LeanTween.value(waelLogo.gameObject, waelLogo.color, targetLogoColor, 0.15f)
+                 .setOnUpdate((Color color) => { waelLogo.color = color; });
+
+        LeanTween.scale(waelLogo.gameObject, Vector3.one * 1.05f, 0.1f)
+                 .setEaseOutQuad()
+                 .setLoopPingPong(1);
+
+        LeanTween.value(gameLogo.gameObject, gameLogo.color, targetGameLogoColor, 0.15f)
+                 .setOnUpdate((Color color) => { gameLogo.color = color; });
+
+        /*        LeanTween.scale(gameLogo.gameObject, Vector3.one * 1.05f, 0.1f)
+                         .setEaseOutQuad()
+                         .setLoopPingPong(1);*/
+
+        isDarkMode = !isDarkMode;
+
+        if (isDarkMode)
+        {
+            logoText.text = "bit<color=#3F3F3F>-<color=#2EAAD2>tac<color=#3F3F3F>-<color=#D22E60>toe";
+        }
+        else
+        {
+            logoText.text = "bit<color=#9D9D9D>-<color=#2EAAD2>tac<color=#9D9D9D>-<color=#D22E60>toe";
+        }
+    }
+
+
 }
